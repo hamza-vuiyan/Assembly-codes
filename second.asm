1a562f947dbfd5004c6710cfc794b524d12b1c87
hamza-vuiyan <hamza.vuiyan@gmail.com>
2025-01-28 00:13:13 +0600

    cleaned the comments in second.asm file

--- second.asm
+++ second.asm
@@ -1,28 +1,24 @@
-.model small   ; //only have one data segment and one code segment in small model
-.stack 100h    ; //defining the stack size, max 64kb for each .stack and 100h means 256bytes
+.model small   
+.stack 100h    
 
-.data  ; //contains all the variables
+.data  
     print db 'Enter a char: $'
-    input db ?          ; //? means nothing initilized in input variable
+    input db ?          
 
 .code
-main:
-    ; Initialize data segment
-    mov bx, @data ; //Loading the address of .data segment not the variables in data segment
-    mov ds, bx
-
-    ; Display prompt
-    mov ah, 09h        ; DOS service to print string
-    lea dx, print     ; Load the address of the prompt string
-    int 21h            ; Call DOS interrupt  
+main proc
+    mov ax, @data 
+    mov ds, ax
+    
+    mov ah, 09h       
+    lea dx, print     
+    int 21h           
     
     mov ah, 01h
     int 21h
-    
-    mov input, al
     
     mov ah, 02h
     lea dl, al
     int 21h
 
-end main
+main endp
